d71478520a395565061cea3ed73a70abe6a1fa9c
Patrick Morrow <git@themorrowgroup.com>
2019-01-29 23:08:53 -0700

    Change default node_modules path.

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ function printStatistics(licenses, depth) {
 
 const scanner = {
 	scan: (directory) => {
-		directory = directory || `./${NODEMODULES_DIRECTORY}/`;
+		directory = directory || `../${NODEMODULES_DIRECTORY}/`;
 
 		console.log(`${COLOR_FOREGROUND_GREEN}License Scanner v1.0.0${COLOR_RESET}`);
 		console.log();
