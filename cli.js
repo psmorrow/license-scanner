5b2e2da4ae8b6421c100c1f93c23c83c70976b38
Patrick Morrow <pat@themorrowgroup.com>
2019-02-23 09:47:00 -0700

    Add parameter support to cli usage.

--- cli.js
+++ cli.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
 
 "use strict";
-require("./index.js").scan();
+
+let directory;
+if (process.argv.length > 2) {
+	directory = process.argv[2];
+}
+
+let format;
+if (process.argv.length > 3) {
+	format = process.argv[3];
+}
+
+console.log(require("./index.js").scan(directory, format));
